d5f8a9caa24c4d8aad687b7e1c0c93afd83dc834
Etienne DEGUINE <etienne-dev-tools@dev-tools.ai>
2022-06-23 18:02:22 -0700

    Change package name to ai.devtools.selenium

--- src/main/java/ai/devtools/sdk/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains the main classes for the dev-tools.ai Smartdriver SDK. These classes provide simple wrappers around existing selenium functionality to seamlessly incorporate dev-tools.ai's powerful element
- * classification technology.
- */
-
-package ai.devtools.sdk;
